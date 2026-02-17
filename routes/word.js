cd2f115d401f1cb726c7fa38d4ec7d1cb7c72b55
Thalia999 <thaliac000@gmail.com>
2026-02-17 12:21:28 -0500

    word

--- routes/word.js
+++ routes/word.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const router = express.Router();
+const fs = require('fs').promises;
+
 
   
 
@@ -11,6 +13,6 @@ res.send('Word Home Page');
 });
 router.get('/wotd', (req, res)=>{
 
-res.send('Word of the Day Page');
+res.render('wotd');
 });
  module.exports = router;
